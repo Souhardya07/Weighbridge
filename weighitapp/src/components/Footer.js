6b4c26b2a432761432ea58a13e62783da0e0023a
SOCODE <149923296+Souhardya07@users.noreply.github.com>
2024-11-13 20:18:31 +0530

    Create Footer.js

---
+++ weighitapp/src/components/Footer.js
@@ -0,0 +1,12 @@
+// src/components/Footer.js
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="bg-blue-800 text-blue-200 text-center py-4">
+      <p>&copy; 2024 WeighIt Co. All Rights Reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
